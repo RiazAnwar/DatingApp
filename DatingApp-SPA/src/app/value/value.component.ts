347c7074a120653e361949bda84c322dbb211f38
RiazAnwar <iamriazanwar@gmail.com>
2020-09-21 17:43:39 +0500

    initial commit

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.scss']
+})
+export class ValueComponent implements OnInit {
+values: any;
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit()
+  {
+    this.getValues();
+  }
+  // tslint:disable-next-line: typedef
+  getValues()
+  {
+    this.http.get('http://localhost:5000/api/values').subscribe(Response => {
+      this.values = Response;
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log(error);
+    } );
+  }
+
+}
